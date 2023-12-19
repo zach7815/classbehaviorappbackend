6c2347feb8fbe5f41c1875ea80b64541c202936e
zachariahkozlowski-best <mcfeegles@gmail.com>
2023-12-19 21:55:07 +0800

    feat: worked on addStudentsToClass feat

--- controllers/teacher.controller.js
+++ controllers/teacher.controller.js
@@ -23,73 +23,61 @@ class TeacherController {
     }
   };
 
-  async addCategoryToSighting2(req, res) {
-    const { sighting_id, categories } = req.body;
-    try {
-      console.log(sighting_id, categories);
-      const newSightingCategory = await this.db.sightingCategories.create({
-        sightingId: sighting_id, //these are snake case in the table, but because of underscored: true, they have to be camel case here
-        categoryId: categories,
-      });
-      return res.json(newSightingCategory);
-    } catch (err) {
-      console.log(err);
-      return res.status(400).json({ error: true, msg: err });
-    }
-  }
-
+  //* bulk create array of objects of students - each obj - teacher_id, class_id, student_id.
+  //* map over currentTeachers create array of objects
   addStudentToClass = async (req, res) => {
     const { student_id, class_id } = req.body;
 
     try {
-      const currentTeachers = await this.db.teacherStudentClasses.findAll({
-        attributes: ['teacher_id'],
-        group: 'teacher_id',
-        where: { class_id: class_id },
-      });
-
-      const teacherIds = currentTeachers.map((teacherObject) => {
-        return teacherObject['dataValues']['teacher_id'];
-      });
-
-      console.log(currentTeachers);
-
-      const AllStudents = await this.db.teacherStudentClasses.findAll({
-        attributes: ['student_id'],
-        group: 'student_id',
-        where: { class_id: class_id },
-      });
-      const studentIds = AllStudents.map((studentObject) => {
-        return studentObject['dataValues']['student_id'];
-      });
-
-      const classPersonal = teacherIds.flatMap((teacher_id) => {
-        return studentIds.map((student_id) => {
-          return {
-            class_id: class_id,
-            teacher_id: teacher_id,
-            student_id: student_id,
-          };
-        });
-      });
-
-      console.log(console.log(classPersonal));
-
-      // console.log('The current teachers are ' + currentTeachers);
-
-      //* bulk create array of objects of students - each obj - teacher_id, class_id, student_id.
-      //* map over currentTeachers create array of objects
-
       const currentClass = await this.db.classes.findByPk(class_id);
-      // console.log(currentClass);
+
       const studentsToAdd = await this.db.students.findByPk(student_id);
-      // console.log(studentsToAdd);
-      // currentClass.addStudent(student);
-      return res.json(currentTeachers);
+      console.log(studentsToAdd);
+
+      if (currentClass && studentsToAdd) {
+        const currentTeachersAndRoles =
+          await this.db.teacherStudentClasses.findAll({
+            attributes: ['teacher_id', 'role_id'],
+            group: ['teacher_id', 'role_id'],
+            where: { class_id: class_id },
+          });
+
+        const teacherAndRoleIds = currentTeachersAndRoles.map(
+          (teacherObject) => {
+            return {
+              teacher_id: teacherObject['dataValues']['teacher_id'],
+              role_id: teacherObject['dataValues']['role_id'],
+            };
+          }
+        );
+
+        const newStudentEntry = teacherAndRoleIds.flatMap(
+          ({ teacher_id, role_id }) => {
+            return {
+              class_id: class_id,
+              teacher_id: teacher_id,
+              role_id: role_id,
+            };
+          }
+        );
+
+        return res.json(currentTeachersAndRoles);
+      } else {
+        return res.send(' The class and/or student do not exist');
+      }
     } catch (err) {
       console.log(err);
       return res.status(400).json({ error: true, msg: err });
     }
+  };
+
+  getAllStudents = async (req, res) => {
+    const AllStudents = await this.db.teacherStudentClasses.findAll({
+      attributes: ['student_id'],
+      group: 'student_id',
+
+      where: { class_id: class_id },
+    });
   };
 
   createClass = async (req, res) => {
