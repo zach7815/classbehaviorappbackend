067e0edec1aac9283a34f26c8a33061c407ab839
zachariahkozlowski-best <mcfeegles@gmail.com>
2023-12-28 16:30:23 +0800

    feat: set up backend for authO authentication from front end. '

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require('./environment');
 const cors = require('cors');
 const express = require('express');
-
+const { auth } = require('express-oauth2-jwt-bearer');
+const app = express();
 const loggerMiddleware = require('./middlewares/logger.middleware');
 const TeacherRouter = require('./routers/teacher.route');
+const PORT = process.env.PORT || 3000;
 
-const app = express();
+const jwtCheck = auth({
+  audience: process.env.AUDIENCE,
+  issuerBaseURL: process.env.ISSUERBASEURL,
+  tokenSigningAlg: process.env.TOKENSIGNINGALG,
+});
+
+app.use(jwtCheck);
 app.use(loggerMiddleware);
 app.use(cors());
 app.use(express.json());
@@ -14,8 +22,6 @@ app.use(express.urlencoded({ extended: false }));
 const routers = [new TeacherRouter()];
 
 routers.forEach((router) => app.use('/', router.router));
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
